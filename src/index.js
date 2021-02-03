6c6fe937a753e5a2825e9779ddd8e9e999c1e3cd
unknown <luck2901@naver.com>
2021-02-03 16:01:18 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -4,18 +4,29 @@ const add = document.getElementById("Add_but");
 const minus = document.getElementById("Minus_but");
 const num = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const reducer = (count = 0, action) =>{
-  if (action.type ==="ADD"){
-    count++;
+  switch(action.type){
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  if (action.type ==="MINUS"){
-    count--;
-  }
-  return count;
 };
 
 const store = createStore(reducer);
 
+const onChange = () =>{
+ num.innerText = store.getState();
+};
+
+store.subscribe(onChange);
+
 console.log(store.getState());
 
-store.dispatch({type:"ADD"});
+add.addEventListener("click",()=>store.dispatch({type:ADD}));
+minus.addEventListener("click",()=>store.dispatch({type:MINUS}));
